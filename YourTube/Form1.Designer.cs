624c0cbf7d7ac1fd27f7449a383a3ad2859d9ac2
Tiegetropfen119 <46197002+Tiegetropfen119@users.noreply.github.com>
2022-07-06 17:03:38 +0200

    Add files via upload

---
+++ YourTube/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace YourTube
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tx_cmdstring = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.credits = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.Location = new System.Drawing.Point(179, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(387, 49);
+            this.textBox1.TabIndex = 29;
+            this.textBox1.Text = "Youtube Url";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Symbol", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(177, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(389, 36);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tx_cmdstring
+            // 
+            this.tx_cmdstring.AutoSize = true;
+            this.tx_cmdstring.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tx_cmdstring.ForeColor = System.Drawing.SystemColors.Control;
+            this.tx_cmdstring.Location = new System.Drawing.Point(12, 335);
+            this.tx_cmdstring.Name = "tx_cmdstring";
+            this.tx_cmdstring.Size = new System.Drawing.Size(27, 30);
+            this.tx_cmdstring.TabIndex = 30;
+            this.tx_cmdstring.Text = "[]";
+            this.tx_cmdstring.Click += new System.EventHandler(this.tx_cmdstring_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Playlist Yes",
+            "Playlist No"});
+            this.comboBox1.Location = new System.Drawing.Point(179, 185);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 31;
+            this.comboBox1.Text = "Playlist Settings";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "MP3",
+            "MP4"});
+            this.comboBox2.Location = new System.Drawing.Point(306, 185);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 32;
+            this.comboBox2.Text = "Format";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.comboBox2.TextUpdate += new System.EventHandler(this.comboBox2_TextUpdate);
+            this.comboBox2.Click += new System.EventHandler(this.comboBox2_Click);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "ID",
+            "ID + Title",
+            "Title"});
+            this.comboBox3.Location = new System.Drawing.Point(433, 185);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 33;
+            this.comboBox3.Text = "Name";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "\"best[height=144]\"",
+            "\"best[height=240]\"",
+            "\"best[height=360]\"",
+            "\"best[height=480]\"",
+            "\"best[height=720]\"",
+            "4k / 1080"});
+            this.comboBox4.Location = new System.Drawing.Point(306, 212);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 34;
+            this.comboBox4.Text = "Pixel";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Symbol", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(12, 402);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(288, 36);
+            this.button2.TabIndex = 35;
+            this.button2.Text = "Move all files to folder \'files\'";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 379);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 20);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "[]";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Segoe UI Symbol", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(500, 402);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(288, 36);
+            this.button3.TabIndex = 37;
+            this.button3.Text = "First Time Install";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox2.Location = new System.Drawing.Point(179, 137);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(387, 42);
+            this.textBox2.TabIndex = 38;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::YourTube.Properties.Resources.video_camera;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(90, 85);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 40;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::YourTube.Properties.Resources.copy;
+            this.pictureBox1.Location = new System.Drawing.Point(572, 138);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 41);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 39;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // credits
+            // 
+            this.credits.AutoSize = true;
+            this.credits.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.credits.ForeColor = System.Drawing.SystemColors.Control;
+            this.credits.Location = new System.Drawing.Point(576, 379);
+            this.credits.Name = "credits";
+            this.credits.Size = new System.Drawing.Size(212, 20);
+            this.credits.TabIndex = 41;
+            this.credits.Text = "GUI by Tiegertropfen119#0001";
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutomaticDelay = 100;
+            this.toolTip1.AutoPopDelay = 60000;
+            this.toolTip1.InitialDelay = 50;
+            this.toolTip1.ReshowDelay = 500;
+            this.toolTip1.ShowAlways = true;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "Add Thumbnail",
+            "Add Metadata",
+            "Add Metadata + Thumbnail"});
+            this.comboBox5.Location = new System.Drawing.Point(433, 212);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(133, 21);
+            this.comboBox5.TabIndex = 42;
+            this.comboBox5.Text = "Metadata";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.credits);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tx_cmdstring);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "YourTube";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label tx_cmdstring;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label credits;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ComboBox comboBox5;
+    }
+}
+
---
+++ YourTube/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Net;
+
+namespace YourTube
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "Youtube Url")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = Color.Gray;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                File.Delete("loader.bat");
+            }
+            catch
+            {
+                
+            }
+            tx_cmdstring.Text = "";
+            tx_cmdstring.Text += " ";
+            tx_cmdstring.Text += textBox1.Text;
+           
+        
+            if (comboBox1.Text == "Playlist Yes")
+            {
+                tx_cmdstring.Text += " --yes-playlist";
+              
+
+            }
+            else
+            {
+                tx_cmdstring.Text += " --no-playlist";
+            }
+            if (comboBox2.Text == "MP3")
+            {
+                tx_cmdstring.Text += " --audio-format mp3 -x";
+               
+
+            }
+            else
+            {
+                tx_cmdstring.Text += " -S res,ext:mp4:m4a --recode mp4";
+                if (comboBox4.Text.Contains("144"))
+                {
+                    tx_cmdstring.Text += " -f ";
+                    tx_cmdstring.Text += comboBox4.Text;
+                }
+                else if (comboBox4.Text.Contains("240"))
+                {
+                    tx_cmdstring.Text += " -f ";
+                    tx_cmdstring.Text += comboBox4.Text;
+                }
+                else if (comboBox4.Text.Contains("360"))
+                {
+                    tx_cmdstring.Text += " -f ";
+                    tx_cmdstring.Text += comboBox4.Text;
+                }
+                else if (comboBox4.Text.Contains("480"))
+                {
+                    tx_cmdstring.Text += " -f ";
+                    tx_cmdstring.Text += comboBox4.Text;
+                }
+                else if (comboBox4.Text.Contains("720"))
+                {
+                    tx_cmdstring.Text += " -f ";
+                    tx_cmdstring.Text += comboBox4.Text;
+                }
+                else if (comboBox4.Text.Contains("Best 4k / 1080"))
+                {
+
+                }
+
+            }
+           
+
+            if (comboBox3.Text == "ID")
+            {
+                tx_cmdstring.Text += " -o %(id)s.%(ext)s";
+            }
+            else if (comboBox3.Text == "Title")
+            {
+                tx_cmdstring.Text += " -o %(title)s.%(ext)s";
+            }
+            else if (comboBox3.Text == "Title + ID")
+            {
+                tx_cmdstring.Text += " -o %(title)s.%(ext)s.%(id)s";
+            }
+            if(comboBox5.Text == "Add Thumbnail")
+            {
+                tx_cmdstring.Text += " --embed-thumbnail";
+            }else if (comboBox5.Text == "Add Metadata")
+            {
+                tx_cmdstring.Text += " --embed-metadata";
+                tx_cmdstring.Text += " --xattrs";
+            }
+            else if (comboBox5.Text == "Add Metadata + Thumbnail")
+            {
+                tx_cmdstring.Text += " --embed-thumbnail";
+                tx_cmdstring.Text += " --embed-metadata";
+                tx_cmdstring.Text += " --xattrs";
+            }
+
+            tx_cmdstring.Text += " --geo-bypass";
+            textBox2.Text = tx_cmdstring.Text;
+
+            string strCmdText;
+            strCmdText = tx_cmdstring.Text;
+                 System.Diagnostics.Process.Start("yt-dlp.exe", strCmdText);
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(this.button1, "Download file or playlist");
+            toolTip1.SetToolTip(this.textBox2, "yt-dlp.exe string");
+            toolTip1.SetToolTip(this.textBox1, "Video or playlist url");
+            toolTip1.SetToolTip(this.comboBox1, "Playlist Yes/No");
+            toolTip1.SetToolTip(this.comboBox2, "Format MP3/MP4");
+            toolTip1.SetToolTip(this.comboBox3, "Name format ID/Title/Title + ID");
+            toolTip1.SetToolTip(this.comboBox4, "Video quality 144/240/360/480/720/Best 4k / 1080");
+            toolTip1.SetToolTip(this.comboBox5, "Add Metadata");
+            toolTip1.SetToolTip(this.pictureBox1, "Copy yt-dlp.exe string");
+            toolTip1.SetToolTip(this.pictureBox2, "Click me ^.^");
+            toolTip1.SetToolTip(this.button2, "Move all *.mp4/*.mp3 files to the files folder");
+            toolTip1.SetToolTip(this.button3, "Download ffmpeg.exe and yt-dlp.exe (you need this)");
+            tx_cmdstring.Visible = false;
+            try
+            {
+                Directory.CreateDirectory("files");
+            }
+            catch
+            {
+
+            }
+            tx_cmdstring.Text = "";
+            label1.Text = "";
+        }
+
+        private void comboBox2_TextUpdate(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void comboBox2_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "MP3")
+            {
+
+                comboBox4.Hide();
+
+            }
+            else
+            {
+
+                comboBox4.Show();
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "MP3")
+            {
+
+                comboBox4.Hide();
+
+            }
+            else
+            {
+
+                comboBox4.Show();
+            }
+        }
+
+        private void tx_cmdstring_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(tx_cmdstring.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string paths = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            
+
+            label1.Text = paths;
+            label1.Text = label1.Text.Replace("YourTube.exe", "");
+            //label1.Text = label1.Text.Replace("\\", "/");
+            string filepath = label1.Text;
+            DirectoryInfo d = new DirectoryInfo(filepath);
+
+            foreach (var file in d.GetFiles("*.mp3"))
+            {
+                try
+                {
+                    Directory.Move(file.FullName, filepath + "\\files\\" + file.Name);
+                }
+                catch { 
+                }
+            
+            }
+
+            foreach (var file in d.GetFiles("*.mp4"))
+            {
+                try
+                {
+                    Directory.Move(file.FullName, filepath + "\\files\\" + file.Name);
+                }
+                catch
+                {
+                }
+
+            }
+
+            foreach (var file in d.GetFiles("*.webm"))
+            {
+                try
+                {
+                    Directory.Move(file.FullName, filepath + "\\files\\" + file.Name);
+                }
+                catch
+                {
+                }
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                File.Delete("ffmpeg.exe");
+                File.Delete("yt-dlp.exe");
+            }
+            catch { 
+            
+            }
+            try
+            {
+                MessageBox.Show("Download size => 115 MB");
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile("https://xamplex.de/data/ffmpeg.exe", "ffmpeg.exe");
+                    client.DownloadFile("https://xamplex.de/data/yt-dlp.exe", "yt-dlp.exe");
+                }
+                MessageBox.Show("Downloaded");
+            }
+            catch
+            {
+                
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(textBox2.Text);
+            }
+            catch
+            {
+
+            }
+            
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://discord.gg/7qE4rTv9fq");
+        }
+    }
+}
